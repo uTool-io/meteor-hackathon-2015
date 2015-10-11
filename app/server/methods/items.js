0eb93da92a3e975e55554e2d700ff97006ae015f
Damir <damir@withpulp.com>
2015-10-11 19:58:00 -0400

    created postItem method

---
+++ app/server/methods/items.js
@@ -0,0 +1,39 @@
+Meteor.methods({
+    postItem: function (itemAttributes) {
+        check(itemAttributes, Object);
+
+        var now = newDate(),
+            user = Meteor.user(),
+            duplicateItem = Items.findOne({$or: [{item: itemAttributes.item}, {image: itemAttributes.image}]});
+
+        if (!user) {
+            throw new Meteor.Error('user-not-logged-in', 'You need to login to post an item.');
+        }
+        if (!itemAttributes.title) {
+            throw new Meteor.Error('item-needs-title', 'You need to create a title for your item.');
+        }
+        if (!itemAttributes.image) {
+            throw new Meteor.Error('item-needs-image', 'You need an image for your item.');
+        }
+        if (duplicateItem) {
+            throw new Meteor.Error('item-attributes-already-exist', 'One or more of your item attributes already exist.');
+        } else {
+            var item = _.extend(_.pick(itemAttributes, 'title', 'image'), {
+                ownerId: user._id,
+                ownerName: user.profile.name,
+                submitted: now,
+                likes: 0
+            });
+
+            var itemId = Items.insert(item);
+
+            return itemId;
+        }
+    },
+    updateItem: function () {
+
+    },
+    removeItem: function () {
+
+    }
+});
