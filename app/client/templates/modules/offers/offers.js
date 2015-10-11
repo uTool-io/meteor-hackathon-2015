087c9dc083c308f26d17bf43e1f5fea4cc07cc91
Damir <damir@withpulp.com>
2015-10-11 08:25:38 -0400

    added more keys to offers helper

--- app/client/templates/modules/offers/offers.js
+++ app/client/templates/modules/offers/offers.js
@@ -10,14 +10,18 @@ Template.offers.helpers({
     offers: function () {
         var allOffers = [],
             offers = Offers.find({}, {sort: {offeredAt: -1}}),
-            userId = Meteor.user()._id;
+            userId = Meteor.user()._id; // @TODO: show offeredItems by userId
 
         offers.forEach(function (offer) {
             var selectedItem = Items.findOne({_id: offer.selectedItemId}),
                 offeredItem = Items.findOne({_id: offer.offeredItemId});
 
+            offer.selectedItemTitle = selectedItem.title;
+            offer.selectedItemImage = selectedItem.image;
             offer.selectedItemOwnerId = selectedItem.ownerId;
             offer.selectedItemOwnerName = selectedItem.ownerName;
+            offer.offeredItemTitle = offeredItem.title;
+            offer.offeredItemImage = offeredItem.image;
             offer.offeredItemOwnerId = offeredItem.ownerId;
             offer.offeredItemOwnerName = offeredItem.ownerName;
 
