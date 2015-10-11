2e95d2f6436b2b291263a3ffea4582c9eeefe44e
Damir <damir@withpulp.com>
2015-10-11 02:41:36 -0400

    updates to item events - wip

--- app/client/templates/modules/items/item.js
+++ app/client/templates/modules/items/item.js
@@ -1,26 +1,38 @@
 Template.item.events({
-    'click .ui.card .image': function (event) {
+    'click #toggleCard': function (event) {
+        event.preventDefault();
+
+        console.log('toggle card helper based on id ' + this._id);
+    },
+    'click #likeItem': function (event) {
+        //@TODO: Create a likeItem button in item template
+        event.preventDefault();
+
+        var selectedItemId = this._id,
+            offeredItemId = 'test';
+
+        Meteor.call('likeItem', itemId, buyerId, function (error) {
+            if (error) {
+                console.error('Like Item method failed: ' + error.reason);
+            } else {
+                console.log('Item ' + itemId + ' added to likes by user ' + buyerId);
+                // show confirmation of like
+            }
+        });
+    },
+    'click #createOffer': function (event) {
+        //@TODO: Move this event to userItem template
         event.preventDefault();
 
         var selectedItemId = this._id,
             offeredItemId = 'test';
 
         Meteor.call('createOffer', selectedItemId, offeredItemId, function (error) {
-           if (error) {
-               console.error('Create Offer method failed: ' + error.reason);
-           } else {
-               console.log('Offered item ' + offeredItemId + 'for selected item ' + selectedItemId);
-           }
+            if (error) {
+                console.error('Create Offer method failed: ' + error.reason);
+            } else {
+                console.log('Offered item ' + offeredItemId + 'for selected item ' + selectedItemId);
+            }
         });
-
-        //@TODO: move to like button event
-        //Meteor.call('likeItem', itemId, buyerId, function (error) {
-        //    if (error) {
-        //        console.error('Like Item method failed: ' + error.reason);
-        //    } else {
-        //        console.log('Item ' + itemId + ' added to likes by user ' + buyerId);
-        //        // show confirmation of like
-        //    }
-        //});
     }
 });
