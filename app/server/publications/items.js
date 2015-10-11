22dd39e9087be0a25c5c6fa1173d59d24335706f
Damir <damir@withpulp.com>
2015-10-10 21:26:40 -0400

    publications updates, removed unused collections

--- app/server/publications/items.js
+++ app/server/publications/items.js
@@ -1,24 +1,8 @@
-Meteor.publishComposite('items', {
-    find: function () {
-        return Items.find();
-    },
-    children: [{
-        find: function (item) {
-            return Meteor.users.find({_id: item.userId}, {fields: {profile: 1}});
-        }
-    }]
+Meteor.publish('items', function(){
+    return Items.find();
 });
 
-Meteor.publishComposite('item', function (id) {
+Meteor.publish('item', function(id) {
     check(id, String);
-    return {
-        find: function () {
-            return Items.find({_id: id});
-        },
-        children: [{
-            find: function (item) {
-                return Meteor.users.find({_id: item.userId}, {fields: {profile: 1}});
-            }
-        }]
-    }
+    return Items.find({_id: id});
 });
