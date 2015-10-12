e29b1fcd084f571d4d0662256e1769173043d21a
Damir <damir@withpulp.com>
2015-10-12 00:32:45 -0400

    counter offer event outlined in todo

--- app/client/templates/modules/offers/offer.js
+++ app/client/templates/modules/offers/offer.js
@@ -5,13 +5,12 @@ Template.offer.events({
         var now = new Date(),
             userId = Meteor.user()._id,
             offerId = this._id,
-
             offer = document.getElementById(offerId),
             cancelAttributes = {
                 cancelledBy: userId,
                 cancelledAt: now
             };
-            console.log (offerId);
+
         Meteor.call('cancelOffer', offerId, cancelAttributes, function (error) {
             if (error) {
                 console.error('cancelOffer method failed: ' + error.reason);
@@ -20,21 +19,34 @@ Template.offer.events({
             }
         });
     },
+    'click .counter.offer.button': function (event) {
+        event.preventDefault();
+
+        var offerId = this._id;
+
+        console.log('counter offer clicked ' + offerId);
+
+        // @TODO: toggle offerModal true
+        // if cancelCounter => offerModal false
+        // if createCounter => call createOffer method
+        //     reverse selectedItemId with offeredItemId
+        //     call cancelOffer method on offerId
+    },
     'click .accept.offer.button': function (event) {
         event.preventDefault();
 
         var now = new Date(),
             userId = Meteor.user()._id,
-            offerId = this._id
+            offerId = this._id;
 
         Meteor.call('acceptOffer', offerId, function (error) {
             if (error) {
                 console.error('acceptOffer method failed: ' + error.reason);
             } else {
-              }
-            });
-
-// @TODO: send to trades/:tradeId + filter out offers if openTrade true
-                FlowRouter.go('trades/:tradeId');
             }
         });
+
+        // @TODO: send to trades/:tradeId + filter out offers if openTrade true
+        FlowRouter.go('trades/:tradeId');
+    }
+});
