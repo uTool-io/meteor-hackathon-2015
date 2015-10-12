7f12603e8ca227041503626a9cacfd1918307a2e
Damir <damir@withpulp.com>
2015-10-12 00:38:30 -0400

    removed unused collections, methods, and modules

--- app/client/templates/modules/items/item.js
+++ app/client/templates/modules/items/item.js
@@ -3,21 +3,5 @@ Template.item.events({
         event.preventDefault();
 
         Session.set('offerModal', this._id);
-    },
-    'click #likeItem': function (event) {
-        //@TODO: Create a likeItem button in item template
-        event.preventDefault();
-
-        var likedItemId = this._id,
-            userId = Meteor.user()._id;;
-
-        Meteor.call('likeItem', likedItemId, userId, function (error) {
-            if (error) {
-                console.error('likeItem method failed: ' + error.reason);
-            } else {
-                console.log('Item ' + likedItemId + ' added to likes by user ' + userId);
-                // show confirmation of like
-            }
-        });
     }
 });
