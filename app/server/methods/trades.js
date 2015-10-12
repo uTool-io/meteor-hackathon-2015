91d02a5bf08146a0e29aadaef14b738defd31bbe
Husam Machlovi <husam@withpulp.com>
2015-10-12 01:06:26 -0400

    trade working

--- app/server/methods/trades.js
+++ app/server/methods/trades.js
@@ -1,24 +1,15 @@
 Meteor.methods({
 
-// @TODO: Trades schema
-// create a createTrade method and call it in acceptOffer method
-// Trades.insert({
-//    offerId: offerId, //see offers for examples on getting data from this id
-//    createdAt: new Date(),
-//    tradeFinal: false  //feel free to change to a better var
-// });
-
  createTrade: function(offerId) {
     check(offerId, String);
+
     var now = new Date();
-        if (offerId) {
+        offer = Offers.findOne(offerId);
+
           Trades.insert({
              offerId: offerId,
              createdAt: now,
              tradeVerdict: false
            });
-         } else {
-              console.log('whats a matta w/ you?')
-          }
       }
   });
