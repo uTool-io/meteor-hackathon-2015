34317d13d9b44ae329eb9b211018779dac7effea
Damir <damir@withpulp.com>
2015-10-18 14:00:25 -0400

    updated ceateOffer event in offerModal

--- src/app/client/templates/modules/offers/offer_modal.js
+++ src/app/client/templates/modules/offers/offer_modal.js
@@ -44,19 +44,8 @@ Template.offerModal.events({
             } else {
                 Session.set('offerModal', false);
                 $('body').removeClass('zeroflow');
+                FlowRouter.go('/barters');
             }
-        });
-
-        Meteor.call('postInsert', post, function(error, result) {
-            // display the error to the user and abort
-            if (error)
-                return throwError(error.reason);
-
-            // show this result but route anyway
-            if (result.postExists)
-                throwError('This link has already been posted');
-
-            Router.go('postPage', {_id: result._id});
         });
     }
 });
