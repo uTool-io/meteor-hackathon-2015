e950052190910b9b267d0394466e460a28391f01
Husam Machlovi <husam@withpulp.com>
2015-10-18 07:18:02 -0400

    contacts wip

--- app/client/templates/views/contacts/contacts.js
+++ app/client/templates/views/contacts/contacts.js
@@ -1,6 +1,15 @@
 Template.contacts.events({
   'click #foobar': function() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+    console.log(navigator.contacts);
+}
+
     console.log('click event works')
+
+    var myContact = navigator.contacts.create({"displayName": "Test User"}, contactSuccess);
+    console.log(myContact);
+
     Meteor.call("foo", function (error) {
       // identify the error
       if (error && error.error === "logged-out") {
