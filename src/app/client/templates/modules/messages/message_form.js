c7de9edba4b2edebe2eae7869d2f545bfb0eed77
Damir <damir@withpulp.com>
2015-10-28 14:05:33 -0400

    sending message wip

--- src/app/client/templates/modules/messages/message_form.js
+++ src/app/client/templates/modules/messages/message_form.js
@@ -1,7 +1,21 @@
 Template.messageForm.events({
-   'submit .ui.message.form': function(event, template) {
-       event.preventDefault();
+    'submit .ui.message.form': function (event) {
+        event.preventDefault();
 
-       console.log('message form submit');
-   }
+        var $message = $(event.target).find('.field .message'),
+            offerId = FlowRouter.getParam('offerId'),
+            offer = Offers.findOne(offerId),
+            receiverId = offer.selectedItemOwner,
+            receiverName = offer.selectedItemOwnerName,
+            messageAttributes = {
+                message: $message.val(),
+                offerId: offerId,
+                receiverId: receiverId,
+                receiverName: receiverName
+            };
+
+        // @TODO: check if current user is either selectedItemOwner or offeredItemOwner
+        // to determine who the receiver of message will be
+        console.log(messageAttributes);
+    }
 });
