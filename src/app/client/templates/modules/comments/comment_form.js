ea4f81fa8b7f2c18a2e7c5646bba28c606fd860c
Damir <damir@withpulp.com>
2015-10-28 16:52:18 -0400

    messages renamed to comments

---
+++ src/app/client/templates/modules/comments/comment_form.js
@@ -0,0 +1,20 @@
+Template.commentForm.events({
+    'submit .ui.comment.form': function (event) {
+        event.preventDefault();
+
+        var offerId = FlowRouter.getParam('offerId'),
+            $comment = $(event.target).find('.field .comment'),
+            commentAttributes = {
+                offerId: offerId,
+                comment: $comment.val()
+            };
+
+        Meteor.call('postComment', commentAttributes, function(error) {
+           if (error) {
+               return console.error(error.reason);
+           } else {
+               $comment.val('');
+           }
+        });
+    }
+});
--- src/app/client/templates/modules/comments/comments.js
+++ src/app/client/templates/modules/comments/comments.js
@@ -1,18 +1,18 @@
-Template.messages.onCreated(function () {
+Template.comments.onCreated(function () {
     var self = this;
     self.autorun(function () {
         // @TODO subscribe by userId
         self.subscribe('items');
         self.subscribe('offers');
         self.subscribe('trades');
-        self.subscribe('messages');
+        self.subscribe('comments');
     });
 });
 
-Template.messages.helpers({
-    messages: function () {
+Template.comments.helpers({
+    comments: function () {
         var offerId = FlowRouter.getParam('offerId');
 
-        return Messages.find({offerId: offerId}, {sort: {createdAt: -1}});
+        return Comments.find({offerId: offerId}, {sort: {createdAt: -1}});
     }
 });
