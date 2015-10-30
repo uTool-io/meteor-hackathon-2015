b61e088c87c8e0108d4b209d760d86d86a99a466
Damir <damir@withpulp.com>
2015-10-30 11:26:22 -0400

    webcam replaced with camera module in post view

--- src/app/client/templates/modules/camera/camera.js
+++ src/app/client/templates/modules/camera/camera.js
@@ -1,45 +1,61 @@
-if (Meteor.isClient) {
-  Session.setDefault('img', null);
+'use strict';
 
-  var getPicture = function(opts) {
-    MeteorCamera.getPicture(opts, function(err, data) {
-      if (err) {
-        console.log('error', err);
-      }
-      if (data) {
-        Session.set('img', data)
-      }
+var getPicture = function (options) {
+    MeteorCamera.getPicture(options, function (error, photo) {
+        if (error) {
+            console.error('error', error);
+        }
+        if (photo) {
+            Session.set('cameraSnap', photo);
+        }
     });
-  };
+};
 
-  Template.cameraEvent.events({
+Template.cameraMDG.onCreated(function () {
+    Session.setDefault('cameraSnap', null);
+});
+
+Template.cameraMDG.onRendered(function () {
+    Session.set('cameraSnap', null);
+});
+
+Template.cameraMDG.events({
     'click #camera-event': function () {
-      getPicture({
-        width: 350,
-        height: 350,
-        quality: 75
-      });
-    }
-  });
-
-  Template.libraryEvent.events({
-    'click #library-event': function () {
-      if (Meteor.isCordova) {
         getPicture({
-          width: 350,
-          height: 350,
-          quality: 75,
-          sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+            width: 350,
+            height: 350,
+            quality: 75
         });
-      } else {
-        alert('Cordova only feature.');
-      }
-    }
-  });
+    },
+    'click #library-event': function () {
+        if (Meteor.isCordova) {
+            getPicture({
+                width: 350,
+                height: 350,
+                quality: 75,
+                sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+            });
+        } else {
+            console.error('Cordova only feature.');
+        }
+    },
+    'click .retake.photo.button': function (event) {
+        event.preventDefault();
 
-  Template.img.helpers({
-    img: function() {
-      return Session.get('img');
+        Session.set('cameraSnap', null);
+    },
+    'click .accept.photo.button': function (event, template) {
+        event.preventDefault();
+
+        var snapData = Session.get('cameraSnap');
+
+        Session.set('acceptedPhoto', snapData);
     }
-  });
-}
+});
+
+Template.cameraMDG.helpers({
+    image: function () {
+        return Session.get('cameraSnap');
+    }
+});
+
