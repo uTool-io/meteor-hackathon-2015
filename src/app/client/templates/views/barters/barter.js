a0255628b82b723b715b715fa3b25358d4bab83e
Damir <damir@withpulp.com>
2015-10-29 20:07:05 -0400

    authorized helper for barter view working

--- src/app/client/templates/views/barters/barter.js
+++ src/app/client/templates/views/barters/barter.js
@@ -11,13 +11,7 @@ Template.barter.helpers({
         var offerId = FlowRouter.getParam('offerId'),
             offer = Offers.findOne(offerId);
 
-        console.log(offer);
-        console.log(offer.selectedItemOwner);
-        console.log(offer.offeredItemOwner);
-        console.log(Meteor.userId());
-        console.log(offer.selectedItemOwner || offer.offeredItemOwner === Meteor.userId());
-
-        if (offer.selectedItemOwner || offer.offeredItemOwner === Meteor.userId()) {
+        if (offer.selectedItemOwner === Meteor.userId() || offer.offeredItemOwner === Meteor.userId()) {
             return true;
         } else {
             return false;
