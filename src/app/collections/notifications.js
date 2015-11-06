dbea86167350d914e54d2f84f75843497b05e32c
Husam Machlovi <husam@withpulp.com>
2015-11-05 22:30:15 -0500

    notifications work - comment notification method

--- src/app/collections/notifications.js
+++ src/app/collections/notifications.js
@@ -9,35 +9,23 @@ Notifications.allow({
 
 createCommentNotification = function(comment) {
 
-    // @TODO: getReceiver() =>
-
     var now = new Date(),
         user = Meteor.user(),
         offer = Offers.findOne(offer.offerId);
 
-    if (currentUser !== selectedItemOwner) {
-        receiverId =
+    if (user !== offer.selectedItemOwner) {
+
+        Notifications.insert({
+            receiverId: offer.selectedItemOwner,
+            receiverName: offer.selectedItemOwnerName,
+            commenterId: offer.offeredItemId,
+            commenterName: offer.offeredItemOwnerName,
+            read: false
+        })
         }
     // if currentUser/senderId !== selectedItemOwner
     //   receiverId = selectedItemOwner
     // else
     //   if currentUser/senderId !== offeredItemOwner
     //      receiverId = offeredItemOwner
-
-    Notifications.insert({
-
-
-        //Needs: offer user + selected user
-        //    - Given I'm on the trade view
-        //    - And I have a new message
-        //    - Then I should be able to click into the message
-        //    - And see:
-        //        - Who sent it
-        //        - The item it's about
-        // */
-        userId: offer.userId,
-        itemId: offer.itemId,
-        read: false
-    });
-    }
 };
