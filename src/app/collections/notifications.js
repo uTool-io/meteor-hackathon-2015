5b9d5ee2f0b0091ceb4a9e2d0f4c510401658d41
Husam Machlovi <husam@withpulp.com>
2015-11-05 22:36:51 -0500

    merge resolution

--- src/app/collections/notifications.js
+++ src/app/collections/notifications.js
@@ -8,22 +8,13 @@ Notifications.allow({
 });
 
 createCommentNotification = function(comment) {
-<<<<<<< HEAD
     var offer = Comments.findOne(offer.offerId);
 
-    // @TODO: getReceiver() =>
-=======
 
     var now = new Date(),
         user = Meteor.user(),
         offer = Offers.findOne(offer.offerId);
 
-<<<<<<< HEAD
-    if (currentUser !== selectedItemOwner) {
-        receiverId =
-        }
->>>>>>> 6f8b215afaeda301591b0e2824d8fb56dea9c39d
-=======
     if (user !== offer.selectedItemOwner) {
 
         Notifications.insert({
@@ -43,36 +34,4 @@ createCommentNotification = function(comment) {
             read: false
         });
     }
-
->>>>>>> notifications
-    // if currentUser/senderId !== selectedItemOwner
-    //   receiverId = selectedItemOwner
-    // else
-    //   if currentUser/senderId !== offeredItemOwner
-    //      receiverId = offeredItemOwner
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-    if (comment.senderId !== offer.selectedItemOwner || comment.senderId !== offer.selectedItemOwner) {
-
-=======
->>>>>>> 6f8b215afaeda301591b0e2824d8fb56dea9c39d
-    Notifications.insert({
-
-
-        //Needs: offer user + selected user
-        //    - Given I'm on the trade view
-        //    - And I have a new message
-        //    - Then I should be able to click into the message
-        //    - And see:
-        //        - Who sent it
-        //        - The item it's about
-        // */
-        userId: offer.userId,
-        itemId: offer.itemId,
-        read: false
-    });
-    }
-=======
->>>>>>> notifications
 };
