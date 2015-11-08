43cf36fca7c402c691d9775a3fb09538666ac198
Husam Machlovi <husam@withpulp.com>
2015-11-07 19:23:20 -0500

    merge resolution

--- src/app/collections/notifications.js
+++ src/app/collections/notifications.js
@@ -6,35 +6,3 @@ Notifications.allow({
                 fieldNames.length == 1 && fieldNames[0] == 'read';
     }
 });
-<<<<<<< HEAD
-
-createCommentNotification = function(comment) {
-    console.log("invocation worked");
-    var offer = Comments.findOne(offer.offerId);
-
-    var now = new Date(),
-        user = Meteor.user(),
-        offer = Offers.findOne(offer.offerId);
-
-    if (user !== offer.selectedItemOwner) {
-
-        Notifications.insert({
-            receiverId: offer.selectedItemOwner,
-            receiverName: offer.selectedItemOwnerName,
-            commenterId: offer.offeredItemId,
-            commenterName: offer.offeredItemOwnerName,
-            read: false
-        });
-    }
-    if (user != offer.offeredItemOwner) {
-        Notifications.insert({
-            receiverId: offer.offeredItemOwner,
-            receiverName: offer.offeredItemOwnerName,
-            commenterId: offer.selectedItemId,
-            commenterName: offer.selectedItemOwnerName,
-            read: false
-        });
-    }
-};
-=======
->>>>>>> notifications
